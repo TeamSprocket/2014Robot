0420a2350ad76e9c5be79eef53e4e275c6b04b8c
Hall Chen <robotics.hchen@gmail.com>
2014-01-11 15:22:27 -0800

    ok

--- src/team/sprocket/subsystems/MecanumDriveTrain.java
+++ src/team/sprocket/subsystems/MecanumDriveTrain.java
@@ -36,7 +36,7 @@ public class MecanumDriveTrain extends Subsystem {
         if(bearing < 0){                                                        //make sure direction is positive
             bearing += 360;                                                     //convert negative direction to positive direction
         }
-        double d = bearing - 90;                                         //we grew up watching ed, edd, 'n eddy
+        double d = 90 - bearing;                                                //we grew up watching ed, edd, 'n eddy
         if(d < 0){                                                              //make sure direction is positive
             d += 360;                                                           //convert negative direction to positive direction
         }
