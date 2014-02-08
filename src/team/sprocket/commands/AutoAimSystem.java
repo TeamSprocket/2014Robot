a4ddbdae26c23ee0c1dfc8af22ab9b2c3ffbd2fd
Hall Chen <robotics.hchen@gmail.com>
2014-02-08 12:45:53 -0800

    updated: -AAS -PPO

--- src/team/sprocket/commands/AutoAimSystem.java
+++ src/team/sprocket/commands/AutoAimSystem.java
@@ -25,6 +25,7 @@ public class AutoAimSystem extends CommandBase {
     protected void execute() {
         if(SmartDashboard.getBoolean("AAS: ", true)){
             distance = sensors.getDistance();
+            SmartDashboard.putString("Distance: ", new Double(sensors.getDistance()).toString());
             currentArmAngle = sensors.getShooterAngle();
             targetArmAngle = currentArmAngle;               //formula to be implemented
             moveShooter();
